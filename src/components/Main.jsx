ad6154a338f13d33cb587f6e38a7d3b3813b8572
This is Mr-DAB <161147802+Deep-Bhanushali@users.noreply.github.com>
2025-07-18 10:47:30 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,3 @@
-// /* eslint-disable no-unused-vars */
 import { useState, useEffect, useRef } from "react";
 import ClaudeRecipe from "./ClaudeRecipe";
 import IngredientList from "./IngredientList";
@@ -7,53 +6,51 @@ import { getRecipeChefClaude } from "./ai";
 const Main = () => {
   const [ingredients, setIngredient] = useState([]);
   const [recipe, setRecipe] = useState("");
-
   const recipeSection = useRef(null);
 
   useEffect(() => {
-    if (recipe !== "" && recipeSection.curret !== null) {
+    if (recipe !== "" && recipeSection.current !== null) {
       recipeSection.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [recipe]);
 
-  console.log("Rendered");
-
   function collectIngredient(formData) {
     const newIngredient = formData.get("ingredient").trim();
-    setIngredient((prevIngredient) => [
-      ...prevIngredient,
-      newIngredient.trim(),
-    ]);
+    if (newIngredient) setIngredient((prev) => [...prev, newIngredient]);
   }
 
   async function getRecipe() {
     const generatedRecipe = await getRecipeChefClaude(ingredients);
     setRecipe(generatedRecipe);
-    console.log(generatedRecipe);
   }
 
   return (
-    <main>
-      {/* <form onSubmit={handleSubmit} className="add-ingredient-form"> */}
-      <form action={collectIngredient} className="add-ingredient-form">
+    <main className="px-[30px] pt-[30px] pb-[10px]">
+      <form
+        action={collectIngredient}
+        className="flex justify-center gap-3 h-[38px]"
+      >
         <input
           type="text"
           placeholder="e.g. oranges"
           aria-label="Add Ingredient"
           name="ingredient"
+          className="rounded-md border border-gray-300 px-[13px] py-[9px] shadow-sm flex-grow min-w-[150px] max-w-[400px]"
         />
-        <button>Add Ingredient</button>
+        <button className="bg-black text-[#fafaf8] w-[150px] text-sm font-medium rounded-md">
+          + Add Ingredient
+        </button>
       </form>
 
-      {ingredients.length ? (
+      {ingredients.length > 0 && (
         <IngredientList
           ingredients={ingredients}
           getRecipe={getRecipe}
           ref={recipeSection}
         />
-      ) : null}
+      )}
 
-      {recipe ? <ClaudeRecipe recipe={recipe} /> : null}
+      {recipe && <ClaudeRecipe recipe={recipe} />}
     </main>
   );
 };
